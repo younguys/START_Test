2105984d955230d0725142224428a0eb06c2cc87
yong <ejrcjsdydrl@naver.com>
2022-05-25 14:59:33 +0900

    0525

--- jun0524.py
+++ jun0524.py
@@ -1,4 +1,11 @@
 #1번문제
-a,b,c,d,e=map(int,input().split())
-total=(a*a+b*b+c*c+d*d+e*e)%10
-print(int(total))
+#a,b,c,d,e=map(int,input().split())
+#total=(a*a+b*b+c*c+d*d+e*e)%10
+#print(int(total))
+#2번문제ord,hex
+#a=input()
+#print(ord(a))
+#3번문제 개행문자앞까지 읽음 sys.stdin.readline().
+#int(input())
+#a=list(map(int,input().split()))
+#print(min(a),max(a))
