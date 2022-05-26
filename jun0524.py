d15e55baf1fbccff81efb6e097bee7dc5033288b
YONG <ejrcjsdydrl@naver.com>
2022-05-26 23:39:24 +0900

    0526

--- jun0524.py
+++ jun0524.py
@@ -34,7 +34,7 @@
 # for j in b:
 #  print(int(a)*j, end='')
 # print()
-#8문제 문자열자체를 비교 보다는 아스키코로 바로 비교하는게 좋을듯
+#8문제 문자열자체를 비교 보다는 아스키코로 바로 비교하는게 좋을듯 ord아스키 문자chr
 #a=input().upper()
 #temp=[]
 #str=[]
@@ -79,18 +79,62 @@
 #else:
 #    print('mixed')
 #12번문제
-a=int(input())
-tempstr=[]
-for i in range(a):
-    tempstr.append(input())
-for i in range(a):
-    count=0
-    totalsum=0
-    for j in range(len(tempstr[i])):
-        if tempstr[i][j]=='O':
-            count=count+1
-            totalsum=totalsum+count
+#a=int(input())
+#tempstr=[]
+#for i in range(a):
+#   tempstr.append(input())
+#for i in range(a):
+#    count=0
+#    totalsum=0
+#    for j in range(len(tempstr[i])):
+#        if tempstr[i][j]=='O':
+#            count=count+1
+#            totalsum=totalsum+count
+#        else:
+#            count=0
+#            continue
+#    print(totalsum)
+#13 set()함수이용하여 중복제거 가능
+#a=[]
+#for i in range(10):
+#    num=int(int(input())%42)
+#    flag=False
+#    for j in a:
+#        if num == j:
+#            flag=True
+#            break
+#        else:
+#            flag=False
+#            continue
+#    if flag == False:
+#        a.append(num)
+#print(len(a))
+#a=[]
+#for i in range(10):
+#    a.append(int(input())%42)
+#print(len(set(a)))
+#14문제
+a=list(input())
+b=[]
+temp=[]
+for i in range(len(a)):
+    b.append(ord(a[i]))
+b.sort()
+b=set(b)
+for i in range(ord('a'),ord('z')+1):
+    flag=False
+    for j in b:
+        if j==i:
+            flag=True
+            temp.append(a.index(chr(i)))
+            break
         else:
-            count=0
+            flag=False
             continue
-    print(totalsum)        
+    if flag==False:
+        temp.append(-1)
+print(" ".join(str(temp)))
+
+
+    
+        
