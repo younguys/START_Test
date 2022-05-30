cc3147cf691b89b59d0e2ffe0fc03f1102ac4c39
yong <ejrcjsdydrl@naver.com>
2022-05-30 15:32:49 +0900

    0530

--- class2.py
+++ class2.py
@@ -25,3 +25,47 @@
 #     else:
 #         print(str(z%x)+'0'+str(int(z/x+1))if len(str(int(z/x+1)))==1 else str(z%x)+str(int(z/x+1)))
 #문제4번
+#문제5번 문제를 잘 읽자
+# a=int(input())
+# count=1
+# endnum=1
+# if a==1:
+#     print(1)
+# else:    
+#     while True:
+#         endnum=count*6+endnum
+#         if endnum>=a:
+#             print(count+1)
+#             break
+#         elif 1000000000<a:
+#             break
+#         else:
+#             count=count+1
+#문제6번
+# num,m=map(int,input().split())
+# temp=list(map(int,input().split()))
+# total=[]
+# for i in range(num):
+#     for j in range(num):
+#         for k in range(num):
+#             if temp[i]!=temp[j] and temp[j]!=temp[k] and temp[i]!=temp[k] and m>=temp[i]+temp[j]+temp[k]:
+#                 total.append(temp[i]+temp[j]+temp[k])
+#             else:
+#                 continue
+#             print(temp[i]+temp[j]+temp[k])
+# print(max(total))
+#문제7번
+# a=input()
+# s=input()
+# total=0
+# for i in range(len(s)):
+#     total=total + (ord(s[i])-96)*pow(31,i)
+# print(total%1234567891)
+#문제8번
+# while True:
+#     s=input()
+#     if s=='0':
+#         break
+#     else:
+#         print('yes'if s[::]==s[::-1] else 'no')
+#문제9번
